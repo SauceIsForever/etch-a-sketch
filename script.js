59c0452a31f6a77daf71bb633782b477dde46cb6
SauceIsForever <164048334+SauceIsForever@users.noreply.github.com>
2025-05-20 13:40:55 -0400

    Width now adjusts to change in grid size

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 const container = document.querySelector(".container");
 
-function myGrid(){
-    for (x=0; x < 256; x++){
-        const square = document.createElement("div");
-        square.className = "square";
-        container.appendChild(square);
-        square.addEventListener('mouseover',  e => e.target.classList.add('newColor'))
+function firstGrid(){
+  for (x=0; x < 256; x++){
+    const square = document.createElement("div");
+    square.className = "square";
+    container.appendChild(square);
+    square.addEventListener('mouseover',  e => e.target.classList.add('newColor'))
     }
 }
 
+function newGrid(){
+const button = document.getElementById("gridButton")
+  button.addEventListener('click', () => {
+    const text = prompt("Select your grid size: ");
+    alert(text);
+
+    let num = Number(text);
+    const width = (100 / num) + '%';
+
+    const containerOne = document.getElementById("containerOne");
+    containerOne.innerHTML = "";
+
+
+    if (num > 0){
+      for (let x=0; x < Math.pow(num, 2); x++){
+      const square = document.createElement("div");
+      square.className = "square";
+      square.style.width = width;
+      container.appendChild(square);
+      square.addEventListener('mouseover',  e => e.target.classList.add('newColor'))
+      } 
+    }  
+  });
+}
+
 function main(){
-  myGrid();
-  //Add to have a pop-up when button is click to ask for grid size
-  //Potentially add event listener
+  firstGrid();
+  newGrid()
 }
 
 main();
